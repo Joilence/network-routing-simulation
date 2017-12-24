bc8c7967afdf94c1f0d118cce6f3edc8510dcdb4
Jonathan Yang <cjyk84719@gmail.com>
2017-12-24 11:15:08 +0800

    io release v1

--- nodejs/control.js
+++ nodejs/control.js
@@ -28,19 +28,3 @@ exports.sendPacket = function sendPacket() {
 }
 
 exports.getDestPort = function (routerName) {}
-
-exports.listenOn = function listenOn(port) {
-  var server = dgram.createSocket('udp4');
-  server.on('listening', () => {
-    const address = server.address();
-    console.log(`服务器监听 ${address.address}:${address.port}`);
-  });
-  server.on('message', (msg, rinfo) => {
-    console.log(`服务器收到：${msg} 来自 ${rinfo.address}:${rinfo.port}`);
-    var packet = JSON.parse(msg);
-    if (packet.protocol === 'ls') {
-      this.LSHandlePacket(packet);
-    }
-  });
-  server.bind(port);
-}
--- nodejs/io.js
+++ nodejs/io.js
@@ -1,11 +1,57 @@
-exports.sendTo = function sendTo(destRouter, msg) {
-  var socket = dgram.createSocket('udp4');
-  socket.send(JSON.stringify(msg), destRouter, '127.0.0.1');
+
+/**
+ * @param {number} dest The port number of dest router
+ * @param {Object} data The data object to send
+ */
+exports.sendTo = function sendTo(dest, data) {
+    // Query route table
+    var entry = this.routeTable.get(dest);
+    var outPort = 0;
+    if (entry) {
+        outPort = entry.toPort;
+    } else {
+        console.log(`${this.logHead()} unknown router ${this.dest()}`.error);
+        return;
+    }
+    // Get out port number and send packet
+    var socket = dgram.createSocket('udp4');
+    socket.send(JSON.stringify(data), outPort, '127.0.0.1');
+    console.log(`${this.logHead()} send ${data.protocol} packet to ${outPort}`);
 }
 
-exports.listen = function listen(params) {
-    
-}
+/**
+ * @param {number} port The router's port
+ */
+exports.listenOn = function listenOn(port) {
+    var server = dgram.createSocket('udp4');
+    server.on('listening', () => {
+      const address = server.address();
+      console.log(`${this.logHead()} now is listening on ${address.address}:${address.port}`);
+    });
+    server.on('message', (msg, rinfo) => {
+        var packet = JSON.parse(msg);
+        console.log(`${this.logHead()} Get ${packet.protocol} packet from ${rinfo.address}:${rinfo.port}`);
+        packetHandler(packet);
+    });
+    server.bind(port);
+  }
 
-exports.packetHandler = function packetHandler(params) {
+/**
+ * @param { {src, dest, protocol, msg} } packet 
+ */
+exports.packetHandler = function packetHandler(packet) {
+    if (packet.protocol === 'ls') {
+        LSStates = JSON.parse(packet.msg);
+        this.LSUpdateRouteTable(LSStates);
+    } else if (packet.protocol === 'dv') {
+        DVStateTable = JSON.parse(package.msg);
+        this.DVUpdateRouteTable(DVStateTable);
+    } else if (packet.protocol === 'data') {
+        if (packet.dest === this.port) {
+            console.log(`${this.logHead()} receive message ${packet.msg}`);
+            //TODO: store received message
+        } else {
+            this.sendTo(dest, packet);
+        }
+    }
 }
