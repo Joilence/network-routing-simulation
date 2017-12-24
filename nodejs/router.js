f51b1e13890db95f22473617bd9d08b24f3d4876
Sirui Chen <632882184@qq.com>
2017-12-24 08:31:40 +0800

    save work

--- nodejs/router.js
+++ nodejs/router.js
@@ -1,4 +1,6 @@
-import { setInterval } from 'timers';
+import {
+    setInterval
+} from 'timers';
 
 var dgram = require('dgram');
 
@@ -42,7 +44,7 @@ class Router {
 
     run(params) {
         listenOn(this.port);
-        setInterval(this.LSBroadcastLinkState.bind(this), 30*1000);
+        setInterval(this.LSBroadcastLinkState.bind(this), 30 * 1000);
     }
 
     LSBroadcastLinkState() {
@@ -69,8 +71,8 @@ class Router {
 
     }
 
-    LSUpdateRouteTable(LSPacket) {
-
+    LSHandlePacket(LSPacket) {
+        this.adjacencyList
     }
 
 
@@ -87,6 +89,10 @@ class Router {
         });
         server.on('message', (msg, rinfo) => {
             console.log(`服务器收到：${msg} 来自 ${rinfo.address}:${rinfo.port}`);
+            var packet = JSON.parse(msg);
+            if (packet.protocol === 'ls') {
+                this.LSHandlePacket(packet);
+            }
         });
         server.bind(port);
     }
